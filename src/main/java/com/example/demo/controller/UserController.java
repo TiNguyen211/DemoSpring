e2330eed8d489557c93e43dcd7c20975db38f8ba
TiNguyen211 <lekhacanhquan211@gmail.com>
2026-03-15 23:24:18 +0700

    Update

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.service.UserService;
 
-// @Controller
-// public class UserController {
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello from controller";
-//     }
-// }
-@RestController
+;
+
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -21,8 +16,21 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "hello";
     }
 }
+
+// @RestController
+// public class UserController {
+//     private UserService userService;
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+//     @GetMapping("")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+// }
