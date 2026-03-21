7a7f822aa07af0b0b9e465cdd3584ed1ddcc18bc
TiNguyen211 <lekhacanhquan211@gmail.com>
2026-03-22 02:28:35 +0700

    Update

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -22,6 +22,14 @@ public class UserController {
         model.addAttribute("TiNguyen", "from controller with model");
         return "hello";
     }
+
+        @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("TiNguyen", "from controller with model");
+        return "admin/user/create";
+    }
 }
 
 // @RestController
